69132f3fb964b62633a30c2ab05677587ca1a953
KristofWork <135259063+KristofWork@users.noreply.github.com>
2024-05-15 22:45:55 +0700

    Перенес первый урок
    Полностью перенес первый урок и задал структуру папок для проекта

---
+++ PythonStart/Модуль 1/1 урок/2. Переменные/Задания/1.py
@@ -0,0 +1,5 @@
+name = "Кантемир"
+day = 2.5
+month = 30
+result = day * month
+print(name, "съедает", result, "килограммов корма в месяц.")
---
+++ PythonStart/Модуль 1/1 урок/2. Переменные/Задания/2.py
@@ -0,0 +1,6 @@
+managers = 7
+price_manager = 100
+regular = 22
+price_regular = 2700
+result = managers * price_manager + regular * price_regular * 2
+print("Итоговая стоимость всех билетов:", result)
